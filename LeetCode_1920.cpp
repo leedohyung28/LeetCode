8a8676f596b899005a3b336885bd7a60e17795cb
leedohyung28 <91038179+leedohyung28@users.noreply.github.com>
2023-07-05 14:57:04 +0900

    Update LeetCode_1920.cpp

--- LeetCode_1920.cpp
+++ LeetCode_1920.cpp
@@ -49,7 +49,8 @@ public:
 };
 
 // 4-line Solution
-ic:
+class Solution {
+public:
     vector<int> buildArray(vector<int>& nums) {
         vector<int> ans;
         for (int i = 0; i < nums.size(); i++){
