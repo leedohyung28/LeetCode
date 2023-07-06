709a11eefa30937836cce3f3a22cbae3fdc1421f
leedohyung28 <91038179+leedohyung28@users.noreply.github.com>
2023-07-06 12:18:35 +0900

    Update LeetCode_1929.cpp

--- LeetCode_1929.cpp
+++ LeetCode_1929.cpp
@@ -45,9 +45,29 @@ class Solution {
     }  
 }
 
-//
+// Best result Code
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+         int output[] = new int[nums.length + nums.length];
+         
+         System.arraycopy(nums, 0, output, 0, nums.length);
+         System.arraycopy(nums, 0, output, nums.length, nums.length);
+         
+         return output;
+    }
+}
 
-//
+// More Simple code
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int ans[] = new int [2*nums.length];
+        for(int i=0; i<nums.length; i++){
+            ans[i] = nums[i];
+            ans[i+nums.length] = nums[i];
+        }
+        return ans;
+    }
+}
 
 
 /*
